17324c31d93ab6beea54549a4c0860a286465dda
Alex Moran <salsicha@gmail.com>
2015-07-07 00:24:29 -0700

    got accelerometer feeding to ui

---
+++ WatchOS/OpenMotionClassifier/OpenMotionClassifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenMotionClassifier
+//
+//  Created by Alex Moran on 7/5/15.
+//  Copyright © 2015 salsicha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
