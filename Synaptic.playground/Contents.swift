01d07b6abc1985707084a503a2b66709534d12e2
Alex Moran <salsicha@gmail.com>
2015-07-18 17:02:50 -0700

    finishing perceptron

--- Synaptic.playground/Contents.swift
+++ Synaptic.playground/Contents.swift
@@ -1,38 +1,65 @@
 //: Playground - noun: a place where people can play
 
-//import UIKit
+import UIKit
 import Foundation
 
 class Architect {
     
     class Perceptron {
-        var inputs:Int = 2
-        var outputs:Int = 2
-        var layers:[Int] = [2]
+        var inputs:Int = 2 // first argument
+        var outputs:Int = 2 // last argument
+        var layers:[Int] = [2] // all the arguments in the middle
+        
+        var input:Layer
+        var hidden = [Layer]()
+        var output:Layer
+        
+        var previous:Layer
         
         init(_ args: Int...) {
             if args.count < 3 {
-                let error = NSException(name:"Error", reason:"not enough layers (minimum 3) !!", userInfo:nil)
-                error.raise()
+                fatalError("Not enough layers (minimum 3) !!")
             }
             
             inputs = args.first! // first argument
             outputs = args.last! // last argument
             layers = Array(args[1...args.count-2]) // all the arguments in the middle
+            
+            input = Layer(inputs)
+            output = Layer(outputs)
+            previous = input
+            
+            // generate hidden layers
+            for level in layers {
+                var layer = Layer(level)
+                hidden.append(layer)
+//                previous.project(layer);
+//                previous = layer;
+            }
+//            previous.project(output);
+            
+            // set layers of the neural network
+//            this.set({
+//                input: input,
+//                hidden: hidden,
+//                output: output
+//            });
         }
         
-        var input = Layer(inputs)
-        var hidden = []
-        var output = Layer(outputs)
-        
-        var previous = input
-        
-        
+        // trainer for the network
+//        var trainer = Trainer();
     }
 }
 
 class Layer {
+    var size = 0
+    var list = []
+    var label:String?
     
+    init(_ size:Int, _ label:String? = nil) {
+        
+    }
+
 }
 
 class Network {
@@ -68,8 +95,8 @@ class Trainer {
 
 
 
-var perceptron = Architect.Perceptron(2, 3, 1);
-perceptron.trainer.XOR();
+var perceptron = Architect.Perceptron(2, 3, 2);
+//perceptron.trainer.XOR();
 
 
 //var test00 = Math.round(perceptron.activate([0, 0]));
